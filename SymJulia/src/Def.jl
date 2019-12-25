3872f966bee0ba0eafc2fe53dde1216a6ff11437
JohnMeadow50 <59218773+JohnMeadow50@users.noreply.github.com>
2019-12-24 23:39:42 -0800

    Update Def.jl

--- SymJulia/src/Def.jl
+++ SymJulia/src/Def.jl
@@ -1,11 +1,9 @@
 macro def(x::Expr)
     if x.head == :(=)
         if typeof(x.args[1]) == Symbol
-            println("in defining a symbol")
-            
-            definevalue(x.args[1], ToMyExpr(x.args[2]))
+            definevalue(x)
         elseif isFunctiondefinition(x)
-            ...
+            definefunction(x)
         end
     else
         println("not defining a Symbol")
@@ -19,12 +17,26 @@ function isSymboldefinition(x::Expr)
     return typeof(x.args[1]) == Symbol
 end
 
-
-function definevalue(x::Symbol, val, env::Env = Sheat)
+function definevalue(expr::Expr, env::Env = Sheat)
+    x, val = expr.args
     if x in env
         throw(AlreadyDefined(string(x)))
     else
         tp = (x, val)
+        push!(env, tp)
+    end
+end
+
+function defineFunction(expr::Expr, env::Env = Sheat)
+    head, defExpr = x.args
+    name = head.args[1]
+    args = convert(Array{Symbol, 1}, head.args[2:end])
+    
+    f = MyFunction(args, defExpr)
+    if name in env
+        throw(AlreadyDefined(string(x)))
+    else
+        tp = (name, f)
         push!(env, tp)
     end
 end
